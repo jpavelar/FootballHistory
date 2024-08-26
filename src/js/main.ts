da3ccede3ee302ee6b8d333f8302361a9b51c0ed
Avelar <joao.avelar@mgwitsolutions.com>
2024-08-25 22:05:43 -0300

    first commit

---
+++ src/js/main.ts
@@ -0,0 +1,54 @@
+const env = require('./.env');
+
+Object.keys(env).forEach(key => {
+  process.env[key] = env[key];
+});
+
+const apiKey = process.env.API_KEY;  
+document.addEventListener('DOMContentLoaded', () => {
+        const apiUrl = 'https://football-highlights-api.p.rapidapi.com/matches?countryCode=SE&limit=20';
+        
+        const options: RequestInit = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': apiKey,
+                'X-RapidAPI-Host': 'football-highlights-api.p.rapidapi.com' as string
+            } as HeadersInit
+        };
+    
+        fetch(apiUrl, options)
+            .then(response => response.json())
+            .then(data => {
+                displayMatches(data);
+            })
+            .catch(error => console.error('Erro ao buscar dados:', error));
+    });
+    
+    function displayMatches(data: any) {
+        const matchesContainer = document.getElementById('matches');
+        if (data && data.data && Array.isArray(data.data)) {
+          data.data.forEach((match: any) => {
+            const matchElement = document.createElement('div');
+            matchElement.className = 'match';
+            matchElement.innerHTML = `
+              <div class="match-details">
+                <img class="logoteam" src="${match.homeTeam.logo || ''}" alt="${match.homeTeam.name || 'Time da Casa'}"> <span class="bold">VS</span> <img class="logoteam" src="${match.awayTeam.logo || ''}" alt="${match.awayTeam.name || 'Time Visitante'}">
+                <p><span class="bold">Data:</span> ${new Date(match.date).toLocaleDateString()}</p>
+                <p><span class="bold">Resultado:</span> ${match.state.score.current || 'N/A'}</p>
+                <p><span class="bold">Descrição:</span> ${match.state.description || 'N/A'}</p>
+                <p><span class="bold">Rodada:</span> ${match.round || 'N/A'}</p>
+                <p><span class="bold">Temporada:</span> ${match.league.season || 'N/A'}</p>
+                <p><span class="bold">Competição:</span> <img src="${match.league.logo || ''}" alt="${match.league.name || 'Competição'}" style="width: 20px; height: 20px;"> ${match.league.name || 'N/A'}</p>
+                
+              </div>
+            `;
+            document.getElementById('matches')?.appendChild(matchElement);
+          });
+        } else {
+          console.error('Dados inválidos');
+        }
+ }
+
+    
+    
+    
